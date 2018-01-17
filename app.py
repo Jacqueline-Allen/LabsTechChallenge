6d260a219b4eefc7d5538a38205f76069dfa5577
Jacqueline Allen <ja3143@columbia.edu>
2018-01-17 11:58:00 -0500

    basic html/css info added + route

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route("/ja3143")
+def uni():
+    return render_template('uni.html')
 
 if __name__ == '__main__':
     app.run()
